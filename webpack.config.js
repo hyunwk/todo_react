c8666cb9acd098ed3848935c9958d9abb46b7579
hyunwk <ab8454@naver.com>
2021-09-15 11:59:49 +0900

    feat: delete babel config

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
-const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path'); // used to resolve absolute path to project's root directory (where web pack.config.js should be located)
 
 module.exports = {
     entry: './src/index.js',
@@ -9,11 +10,17 @@ module.exports = {
     module: {
         rules: [
             {
-                test: /\.js$/,
+                test: /\.js$/, // property identifies which file or files should be transformed.
                 exclude: /node_modules/,
-                use: 'babel-loader',
+                use: {
+                    loader: 'babel-loader', // property indicates which loader should be used to do the transforming.
+                    options: {
+                        presets: ['@babel/preset-react'],
+                    }
+                },
             },
         ],
     },
+    plugins: [new HtmlWebpackPlugin()],
     mode: 'production',
 };
