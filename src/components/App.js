bcb580082caa0bf35f00f8d98df7812c71cfe030
hyunwk <ab8454@naver.com>
2021-09-27 19:52:44 +0900

    feat: init

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App = class extends React.Component {
+  render() {
+    return (
+        <h1>React Markdown Note 🌙</h1>
+    );
+  }
+};
+
+export default App;
--- webpack.config.js
+++ webpack.config.js
@@ -1,37 +1,30 @@
+const webpack = require('webpack')
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const path = require('path'); // used to resolve absolute path to project's root directory (where web pack.config.js should be located)
-
 module.exports = {
-    entry: './src/index.js',
-    output: {
-        filename: 'main.js',
-        path: path.resolve(__dirname, 'dist'),
-    },
-    module: {
-        rules: [
-            {
-                test: /\.js$/, // property identifies which file or files should be transformed.
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader', // property indicates which loader should be used to do the transforming.
-                    options: {
-                        presets: ['@babel/preset-react'],
-                    }
-                },
-            },
-            {
-                test: /\.css$/,   //.css 확장자로 끝나는 모든 파일
-                use: ['style-loader', 'css-loader']
-            }
-        ],
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: 'public/index.html',
-        }),
+  mode: 'development',
+  devtool: 'eval-source-map',
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    clean: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'public/index.html',
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+          },
+        },
+      },
     ],
-    mode: 'development',
-    performance: {
-        hints: false
-    }
+  },
 };
